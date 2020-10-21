bc9e0891c493a83f35b53b1c8aa188d4e58d2e7c
viieja <msantoro@fi.uba.ar>
2020-10-20 21:03:28 -0300

    add functions pila.c - missing the TESTEOS

--- tps/pila/pila.c
+++ tps/pila/pila.c
@@ -4,6 +4,10 @@
 
 /* Definición del struct pila proporcionado por la cátedra.
  */
+
+#define SIZE_BASE 10
+#define CTE_REALLOC 2
+
 struct pila {
     void **datos;
     size_t cantidad;   // Cantidad de elementos almacenados.
@@ -15,3 +19,47 @@ struct pila {
  * *****************************************************************/
 
 // ...
+
+pila_t *pila_crear(void){
+    pila_t *p = malloc(sizeof(pila_t));
+    if (p == NULL) return NULL;
+    p->cantidad = 0;
+    p->datos = 0;
+    p->datos = malloc(sizeof(void *) * SIZE_BASE);
+    if (p->datos == NULL) {
+        free(p);
+        return NULL;
+    }
+    return p;
+}
+
+void pila_destruir(pila_t *pila){
+    for (size_t i = 0; i < pila->cantidad; i++)
+        free(pila->datos[i]);
+    free(pila);
+}
+
+bool pila_esta_vacia(const pila_t *pila){
+    return pila->cantidad;
+}
+
+bool pila_apilar(pila_t *pila, void *valor){
+    if (pila->cantidad >= pila->capacidad){
+        void *aux = realloc(pila->datos, sizeof(void *) * pila->capacidad * CTE_REALLOC);
+        if (aux == NULL) return false;
+        pila->datos = aux;
+        pila->capacidad *= CTE_REALLOC;
+    }
+    pila->datos[pila->cantidad++] = valor;
+    return true;
+}
+
+void *pila_ver_tope(const pila_t *pila){
+    if (pila->capacidad == 0) return NULL;
+    return pila->datos[pila->capacidad];
+}
+
+void *pila_desapilar(pila_t *pila){
+    if (pila->cantidad == 0) return NULL;
+    return pila->datos[pila->capacidad--];
+}
--- tps/pila/pruebas_pila.c
+++ tps/pila/pruebas_pila.c
@@ -1,16 +1,30 @@
 #include "pila.h"
 #include "testing.h"
 
+#define NULL (void *)0
 
 static void prueba_pila_vacia(void) {
-    pila_t *pila = pila_crear();
-    // print_test( ... pila_esta_vacia(pila) ... );
+    pila_t *p = pila_crear();
+    print_test("la pila fue creada", p != NULL);
+    print_test("la pila esta vacia", pila_esta_vacia(p));
 }
 
+static void prueba_crear_destruir(void) {
+    pila_t *p = pila_crear();
+    print_test("la pila fue creada", p != NULL);
+}
+
+static void prueba_apilar_desapilar(void) {
+    pila_t *p = pila_crear();
+    int *elem;
+    
+
+}
 
 void pruebas_pila_estudiante() {
     prueba_pila_vacia();
-    // ...
+    prueba_crear_destruir();
+    prueba_apilar_desapilar();
 }
 
 
