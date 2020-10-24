f8ae7ed7825e61fe957e7d88564c206c2be93407
viieja <msantoro@fi.uba.ar>
2020-10-24 10:35:56 -0300

    recursividad

--- apuntes/invertir_cola_recursiva.c
+++ apuntes/invertir_cola_recursiva.c
@@ -7,3 +7,19 @@ void invertir_cola(cola_t *cola)
     invertir_cola(cola);
     cola_encolar(cola, dato);
 }
+
+void cola_invertir(cola_t *cola)
+{
+    nodo_t *actual = cola->prim;
+    nodo_t *anterior = NULL;
+    nodo_t *siguiente = actual ? actual->sig : NULL;
+    cola->ult = cola->prim;
+    while (actual)
+    {
+        acutal->sig = anterior;
+        anterior = actual;
+        actual = siguiente;
+        siguiente = actual ? actual->sig : NULL;
+    }
+    cola->prim = anterior;
+}
