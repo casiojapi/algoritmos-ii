b9adfa225bcebf70debd22b980c0f2c90d7ad559
viieja <msantoro@fi.uba.ar>
2020-11-03 20:00:23 -0300

    add condiciones en .h

--- tps/lista/lista.h
+++ tps/lista/lista.h
@@ -16,42 +16,78 @@ typedef struct lista lista_t;
 
 
 /* *****************************************************************
- *                    PRIMITIVAS DE LA lista
+ *                    PRIMITIVAS DE LA LISTA
  * *****************************************************************/
 
 // Crea una lista.
 // Post: devuelve una nueva lista vacía.
 lista_t *lista_crear(void);
 
-// Destruye la lista.
-// Pre: la lista fue creada.
-// Post: se eliminaron todos los elementos de la lista.
-void lista_destruir(lista_t *lista);
-
 // Devuelve verdadero si la lista no tiene elementos alistados, false en caso contrario.
 // Pre: la lista fue creada.
 bool lista_esta_vacia(const lista_t *lista);
 
-// Agrega un nuevo elemento a la lista. Devuelve falso en caso de error.
+// Agrega un nuevo elemento en el primer lugar de la lista. Devuelve falso en caso de error.
 // Pre: la lista fue creada.
-// Post: se agregó un nuevo elemento a la lista, valor es el nuevo tope.
-bool lista_alistar(lista_t *lista, void *valor);
+// Post: se agregó un nuevo elemento a la lista, valor se encuentra al final
+// de la lista.
+bool lista_insertar_primero(lista_t *lista, void *dato);
 
-// Obtiene el valor del tope de la lista. Si la lista tiene elementos,
-// se devuelve el valor del tope. Si está vacía devuelve NULL.
+// Agrega un nuevo elemento en el ultimo lugar de la lista. Devuelve falso en caso de error.
 // Pre: la lista fue creada.
-// Post: se devolvió el valor del tope de la lista, cuando la lista no está
-// vacía, NULL en caso contrario.
-void *lista_ver_tope(const lista_t *lista);
+// Post: se agregó un nuevo elemento a la lista, valor se encuentra al final
+// de la lista.
+bool lista_insertar_ultimo(lista_t *lista, void *dato);
 
-// Saca el elemento tope de la lista. Si la lista tiene elementos, se quita el
-// tope de la lista, y se devuelve ese valor. Si la lista está vacía, devuelve
-// NULL.
+// Saca el primer elemento de la lista. Si la lista tiene elementos, se quita el
+// primero de la lista, y se devuelve su valor, si está vacía, devuelve NULL.
 // Pre: la lista fue creada.
-// Post: si la lista no estaba vacía, se devuelve el valor del tope anterior
-// y la lista contiene un elemento menos.
-void *lista_desalistar(lista_t *lista);
+// Post: se devolvió el valor del primer elemento anterior, la lista
+// contiene un elemento menos, si la lista no estaba vacía.
+void *lista_borrar_primero(lista_t *lista);
 
+// Obtiene el valor del primer elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el primer elemento de la lista, cuando no está vacía.
+void *lista_ver_primero(const lista_t *lista);
+
+// Obtiene el valor del ultimo elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el ultimo elemento de la lista, cuando no está vacía.
+void *lista_ver_ultimo(const lista_t* lista);
+
+// Devuelve el tamaño de la lista.
+// Pre: la lista fue creada.
+size_t lista_largo(const lista_t *lista);
+
+// Destruye la lista. Si se recibe la función destruir_dato por parámetro,
+// para cada uno de los elementos de la lista llama a destruir_dato.
+// Pre: la lista fue creada. destruir_dato es una función capaz de destruir
+// los datos de la lista, o NULL en caso de que no se la utilice.
+// Post: se eliminaron todos los elementos de la lista.
+void lista_destruir(lista_t *lista, void (*destruir_dato)(void *));
+
+/* *****************************************************************
+ *                    PRIMITIVAS ITERADOR INTERNO
+ * *****************************************************************/
+
+void lista_iterar(lista_t *lista, bool visitar(void *dato, void *extra), void *extra);
+
+/* *****************************************************************
+ *                    PRIMITIVAS ITERADOR EXTERNO
+ * *****************************************************************/
+
+typedef struct lista_iter lista_iter_t;
+
+lista_iter_t *lista_iter_crear(lista_t *lista);
+bool lista_iter_avanzar(lista_iter_t *iter);
+void *lista_iter_ver_actual(const lista_iter_t *iter);
+bool lista_iter_al_final(const lista_iter_t *iter);
+void lista_iter_destruir(lista_iter_t *iter);
+bool lista_iter_insertar(lista_iter_t *iter, void *dato);
+void *lista_iter_borrar(lista_iter_t *iter);
 
 /* *****************************************************************
  *                    PRUEBAS PARA LA lista
