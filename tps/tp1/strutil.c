2986534125b23d6d83c073d8d2df4a40fe198ec1
viieja <msantoro@fi.uba.ar>
2020-11-19 12:18:24 -0300

    perfect strutil

--- tps/tp1/strutil.c
+++ tps/tp1/strutil.c
@@ -3,18 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#define MAX_IND 100
+#define MAX_IND 100000
 
 char *substr(const char *str, size_t n) {
     char* c = calloc(n + 1, sizeof(char));
     if (!c) return NULL;
     strncpy(c, str, n);
+    //c[n] = '\0';
     return c;
 }
 
 char **split(const char *str, char sep) {
-    if (!str) return NULL;
+    if (!str) {
+        printf("NULLLLLLLLLLLLLLLL\n");
+        return NULL;
+    } 
     size_t n = strlen(str);
     size_t cadenas = 0;
     char** cc = NULL;
@@ -26,7 +31,7 @@ char **split(const char *str, char sep) {
             indices[cadenas] = i + 1;
         }
     }
-    indices[++cadenas] = n+ 1;
+    indices[++cadenas] = n + 1;
     cc = malloc(sizeof(char*) * (cadenas + 1));
     if (!cc) return NULL;
     for (size_t i = 0; i < cadenas; i++) {
@@ -38,28 +43,31 @@ char **split(const char *str, char sep) {
 
 char *join(char **strv, char sep) {
     size_t strs = 0, mem = 0;
-    if (!strv) return NULL;
+    if (!strv) return strdup("");
+
+    char no_null = 1;
+    if (sep == '\0') {
+        no_null = 0;
+    }
 
     size_t lens[MAX_IND];
     for (size_t i = 0; strv[i]; i++) {
         lens[strs] = strlen(strv[i]) + 1;
         mem += lens[strs++];
     }
-    if (!mem) {
-        char* c = malloc(sizeof(char));
-        if (!c) return NULL;
-        *c = '\0';
-        return c;
-    }
+    if (!mem)
+        return strdup("");
+
     char* c = calloc(mem, sizeof(char));
     if (!c) return NULL;
     size_t index = 0;
     for (size_t i = 0; i < strs; i++) {
         strncpy(c + index, strv[i], lens[i]);
-        index += lens[i] -1;
-        strncpy(c + index++, &sep, 1);
+        index += lens[i] - 1;
+        if (no_null)
+            strncpy(c + index++, &sep, 1);
     }
-    c[index-1] = '\0';
+    c[index - no_null] = '\0';
     return c;
 }
 
