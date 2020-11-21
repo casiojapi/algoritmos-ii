8c77355c6b4b5cef30279d574b19b3b625b3b443
viieja <msantoro@fi.uba.ar>
2020-11-21 14:43:13 -0300

    avanza el hash muy de a poco

--- tps/hash/hash.c
+++ tps/hash/hash.c
@@ -17,14 +17,14 @@ typedef struct hash_memb {
     char* clave;
     void* dato;
     estado_t estado;
-
 } hash_memb_t;
 
 
 struct hash {
     hash_memb_t* tabla;
     size_t capacidad, cantidad, borrados;
-};
+    hash_destruir_dato_t destruir_dato;
+};  
 
 
 // Voy a usar djb2
@@ -49,6 +49,7 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato) {
         return NULL;
     }
     h->capacidad = CAP_INIC;
+    h->destruir_dato = destruir_dato;
     return h;
 }
 
@@ -58,5 +59,52 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato) {
  * Post: Se almacenó el par (clave, dato)
  */
 bool hash_guardar(hash_t *hash, const char *clave, void *dato) {
+    // if cant / cap >= 0.7 redimensiona * 2
+    size_t pos = f_hash(clave) % hash->capacidad;
+    while (hash->tabla[pos].estado == OCUPADO) {
+        pos *= pos;
+        if (pos > hash->capacidad)
+            pos %= hash->capacidad;
+    }
     
+}
+
+
+/* Borra un elemento del hash y devuelve el dato asociado.  Devuelve
+ * NULL si el dato no estaba.
+ * Pre: La estructura hash fue inicializada
+ * Post: El elemento fue borrado de la estructura y se lo devolvió,
+ * en el caso de que estuviera guardado.
+ */
+void *hash_borrar(hash_t *hash, const char *clave);
+
+/* Obtiene el valor de un elemento del hash, si la clave no se encuentra
+ * devuelve NULL.
+ * Pre: La estructura hash fue inicializada
+ */
+void *hash_obtener(const hash_t *hash, const char *clave);
+
+/* Determina si clave pertenece o no al hash.
+ * Pre: La estructura hash fue inicializada
+ */
+bool hash_pertenece(const hash_t *hash, const char *clave);
+
+/* Devuelve la cantidad de elementos del hash.
+ * Pre: La estructura hash fue inicializada
+ */
+size_t hash_cantidad(const hash_t *hash);
+
+/* Destruye la estructura liberando la memoria pedida y llamando a la función
+ * destruir para cada par (clave, dato).
+ * Pre: La estructura hash fue inicializada
+ * Post: La estructura hash fue destruida
+ */
+void hash_destruir(hash_t *hash) {
+    for (size_t i = 0; i < hash->capacidad;  i++) {
+        if (hash->tabla[i].estado == OCUPADO && hash->destruir_dato) {
+            hash->destruir_dato(hash->tabla[i].dato);
+        }
+    }
+    free(hash->tabla);
+    free(hash);
 }
