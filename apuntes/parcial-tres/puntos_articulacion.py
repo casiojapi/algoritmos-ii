ef5cb9d9dc1c3955f6088474eb41f1b01a338e2e
viieja <msantoro@fi.uba.ar>
2020-12-19 18:23:38 -0300

    add tarjan implementacion

--- apuntes/parcial-tres/puntos_articulacion.py
+++ apuntes/parcial-tres/puntos_articulacion.py
@@ -15,3 +15,24 @@ def puntos_articulacion(grafo, v, visitados, es_raiz, orden, mas_bajo, p_art): #
     if es_raiz and hijos > 1:
         p_art.add(v)
     
+def tarjan(grafo, v, visitados, pila, apilados, orden, mas_bajo, cfcs, *indice):
+    visitados.add(v)
+    pila.apilar(v)
+    apilados.add(v)
+    mas_bajo[v] = orden[v]
+    for w in grafo.adyacentes(v):
+        if w not in visitados:
+            orden[w] = *indice + 1
+            *indice += 1
+            tarjan(grafo, w, visitados, pila, apilados, orden, mas_bajo, cfcs)
+            mas_bajo[v] = min(mas_bajo[v], mas_bajo[w])
+        else if w in apilados:
+            mas_bajo[v] = min(mas_bajo[v], orden[w])
+    if mas_bajo[v] == orden[v]
+        nueva_cfc = []
+        do:
+            w = pila.desapilar()
+            apilados.remove(w)
+            nueva_cfc.append(w)
+        while w != v
+        cfcs.append(nueva_cfc)
